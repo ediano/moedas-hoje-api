88559e4790e8872561636c64a13ce3c8e68273b7
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2021-05-22 12:51:15 -0400

    add new exchange braziliex.com

---
+++ src/utils/serialize/braziliex.ts
@@ -0,0 +1,40 @@
+import { MoedasHojeApiResponse } from '@/utils/serialize'
+
+const timestamp = Date.parse(String(new Date())) / 1000
+
+type BraziliexTicker = {
+  [key: string]: {
+    last: number
+    highestBid24: number
+    lowestAsk24: number
+    market: string
+  }
+}
+
+type BraziliexApiResponse = { data: BraziliexTicker }
+
+type Braziliex = MoedasHojeApiResponse<BraziliexApiResponse>
+
+export const serializeBraziliex = ({
+  source,
+  assets,
+  delay,
+  data
+}: Braziliex): MoedasHojeApiResponse => {
+  return {
+    source,
+    assets,
+    delay,
+    data: Object.keys(data)
+      .map(item => {
+        return {
+          ask: String(data[item].last),
+          symbol: data[item].market.toUpperCase(),
+          high24h: String(data[item].highestBid24),
+          low24h: String(data[item].lowestAsk24),
+          timestamp
+        }
+      })
+      .filter(item => item.ask)
+  }
+}
